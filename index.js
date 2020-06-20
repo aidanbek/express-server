eec592d8b22c4dae512ee0ff2c7e4b6a11a7fea8
Aidanbek Kalymbekov <aidanbek@protonmail.com>
2020-06-20 17:24:04 +0600

    add handlebars, default views and bootstrap

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 require('dotenv').config();
 const express = require('express');
-const logger = require('./helpers/logger');
-const requestLogger= require('./helpers/requestLogger');
-const compression = require('compression');
 const app = express();
+const logger = require('./helpers/logger');
+const requestLogger = require('./helpers/requestLogger');
+const compression = require('compression');
+const expresshbs = require('express-handlebars');
+const path = require('path');
+
+app.engine('.hbs', expresshbs({
+    extname: '.hbs',
+    defaultLayout: 'main'
+}));
+app.set('view engine', '.hbs');
 
 app.use(compression());
 app.use(requestLogger);
 
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/css', express.static(path.join(__dirname, 'public')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist/')));
+
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.render('home');
 });
 
 app.listen(3000, function () {
